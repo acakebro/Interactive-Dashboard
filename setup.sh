23e90b2f57b5b1758980128f292548bfa1a533f1
757cyh <67052883+acakebro@users.noreply.github.com>
2022-07-06 15:42:46 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,19 @@
 mkdir -p ~/.streamlit/
-
+ 
 echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
-
+ 
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+[theme]
+primaryColor = '#E694FF'
+backgroundColor = '#00172B'
+secondaryBackgroundColor = '#0083B8'
+textColor = '#FFF'
+font = 'sans serif'
 " > ~/.streamlit/config.toml
